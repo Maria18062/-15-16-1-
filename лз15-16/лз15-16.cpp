7db6b3c06b22e8167aa3a828ba9ae56a48a77ba1
User <User@WIN-110HH93FM7Q>
2024-11-20 20:50:58 +0200

    Добавьте файлы проекта.

---
+++ лз15-16/лз15-16.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream> // для роботи з файлами
+#include <cmath> // для обчислення квадратів
+#include <Windows.h>
+using namespace std;
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	// Відкриваємо файл chusla.txt для читання
+	ifstream inputFile("chusla.txt");
+	// Перевіряємо, чи файл успішно відкрився
+	if (!inputFile) {
+		cerr << "Не вдалося відкрити файл chusla.txt" << endl;
+		return 1;
+	}
+	int number; // Змінна для зчитування чисел з файлу
+	int sumOfSquares = 0; // Змінна для суми квадратів додатних чисел
+	// Читання даних з файлу
+	while (inputFile >> number) {
+		// Перевіряємо, чи число додатне
+		if (number > 0) {
+			// Додаємо квадрат числа до загальної суми
+			sumOfSquares += pow(number, 2);
+		}
+	}
+	// Закриваємо файл
+	inputFile.close();
+	// Виводимо результат
+	cout << "Сума квадратів додатних чисел: " << sumOfSquares << endl;
+	return 0;
+}
